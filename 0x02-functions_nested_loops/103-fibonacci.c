0e210c7a2ba14d68245c42e863f2327f74c27cea
lloydwein <oge.otukpa@gmail.com>
2023-03-21 14:45:50 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * in a fibbonacci sequence, followed by a new line
+ * Description: a program
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			total_sum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+
+	return (0);
+}
