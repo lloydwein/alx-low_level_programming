9f53231b4a245f825cdbd20a21aa329da3e216a8
lloydwein <oge.otukpa@gmail.com>
2023-03-23 13:01:24 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,12 @@
 
 /**
  * mul - multiplies two integers
+ *
  * Description: function
  * @a: first integer
  * @b: second integer
- * Result: prodduct of two integers
+ *
+ * Retuen: product of two integers
  */
 
 int mul(int a, int b)
