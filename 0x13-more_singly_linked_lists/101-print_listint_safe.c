7449d56816789890fd6ba3e3a45f25abdd5071d6
lloydwein <oge.otukpa@gamil.com>
2023-05-02 11:00:30 -0700

    Write a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,80 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t list, even if it loops
- * @head: list to print
+ * free_address_list - frees a listint_addr list
+ * @head: a pointer to the head
  *
- * Return: Number of nodes
+ * This function frees a linked list of nodes, starting from the head.
  */
-size_t print_listint_safe(const listint_t *head)
+void free_address_list(listint_addr *head)
 {
-	const listint_t *current, *check;
-	size_t node_count = 0, index;
+	listint_addr *current = head;
 
-	current = head;
 	while (current)
 	{
-		check = head;
-		for (index = 0; index < node_count; index++)
-		{
-			if (check <= current)
-			{
-				printf("-> [%p] %d\n", (void *)current,
-						current->n);
-				return (node_count);
-			}
-			check = check->next;
-		}
-		printf("[%p] %d\n", (void *)current, current->n);
-		current = current->next;
-		node_count++;
+		listint_addr *next = current->next;
+
+		free(current);
+		current = next;
 	}
-	return (node_count);
+}
+
+/**
+ * add_nodeaddr - Adds a new node to a linked list of addresses.
+ * @head: A pointer to the head of the list.
+ * @addr: The address to store in the new node.
+ *
+ * Return: If memory allocation fails - exit with status 98.
+ *         Otherwise - a pointer to the new node.
+ */
+listint_addr *add_nodeaddr(listint_addr **head, const listint_t *addr)
+{
+	listint_addr *new_node;
+
+	new_node = malloc(sizeof(listint_addr));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		free_address_list(*head);
+		exit(98);
+	}
+
+	new_node->address = addr;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
+
+/**
+ * print_listint_safe - prints a listint_t list, even if it has a loop
+ * @head: a pointer to the head of the list
+ * Return: the number of nodes in the list
+ **/
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t n = 0;
+	listint_addr *addresses, *current_address;
+
+	addresses = NULL;
+	while (head)
+	{
+		current_address = addresses;
+		while (addresses)
+		{
+			if (addresses->address == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_address_list(current_address);
+				return (n);
+			}
+			addresses = addresses->next;
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		addresses = current_address;
+		add_nodeaddr(&addresses, head);
+		head = head->next;
+		n++;
+	}
+	free_address_list(addresses);
+	return (n);
 }
