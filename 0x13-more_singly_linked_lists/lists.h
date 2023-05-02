efd76fd7e43fdbd98cf6c5821f25ff2dc69b243b
lloydwein <oge.otukpa@gamil.com>
2023-05-02 10:59:48 -0700

    header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -36,4 +36,18 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
+/**
+ * struct listint_address - singly linked list clone storing address
+ * @next: points to the next node
+ * @address: address from another list
+ * Description: singly linked list node structure
+ * that strore address from another
+ */
+typedef struct listint_address
+{
+	const struct listint_s *address;
+	struct listint_address *next;
+} listint_addr;
+void free_address_list(listint_addr *head);
+listint_addr *add_nodeaddr(listint_addr **head, const listint_t *addr);
 #endif
